f6c86b5c68b9d99224a23951d55630593a6578bc
ncc <ncc>
2017-02-22 09:48:57 -0800

    Teacher class finished

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,6 +1,16 @@
-
+#include <iostream>
+#include "student.h"
+#include "Teacher.h"
+using namespace std;
 
 int main()
 {
+	Teacher james = Teacher("James");
+	Teacher bob = Teacher("Bob");
+	cout << james.getName() << endl;
+	cout << bob.getName() << endl;
+
+	getchar();
+
 	return 0;
 }
